d7afef378a9ad1a898133c4f721253b898a16389
westatITS <lamontbigham@westat.com>
2025-02-03 11:21:39 -0500

    Delete Windows 11 Upgrade - Welcome directory

--- Windows 11 Upgrade - Welcome/story_content/user.js
+++
@@ -1,8 +0,0 @@
-window.InitUserScripts = function()
-{
-var player = GetPlayer();
-var object = player.object;
-var addToTimeline = player.addToTimeline;
-var setVar = player.SetVar;
-var getVar = player.GetVar;
-};
